75c61ade426b819a679233e851c7dbbd57015731
Cheng Sheng <jeru.sheng@gmail.com>
2024-12-13 00:44:01 +0100

    Minor improvements.

--- codeforces/p1776f.kt
+++ codeforces/p1776f.kt
@@ -25,38 +25,29 @@ fun constructPlan(n: Int, edges: List<Pair<Int, Int>>, a: Int, b: Int): Pair<Int
 }
 
 fun constructPlanFromClique(n: Int, edges: List<Pair<Int, Int>>): Pair<Int, List<Int>> {
-    return Pair(3, edges.map{(u, v) ->
-        if (u == 1 && v == 2) {
-            1
-        } else if (u == 1 || v == 1) {
-            2
-        } else if (u == 2 || v == 2) {
-            3
-        } else {
-            1
-        }
-    }.toList())
+    return Pair(3, edges.map{(u, v) -> when {
+        u == 1 && v == 2 -> 1
+        u == 1 || v == 1 -> 2
+        u == 2 || v == 2 -> 3
+        else -> 1
+    }}.toList())
 }
 
 fun main() {
     val numCases = readln().toInt()
     for (i in 1..numCases) {
         val (n, m) = readPairInts()
-        val edges = (1..m).map{readPairInts()}.map{
-            if (it.first > it.second) {
-                Pair(it.second, it.first)
-            } else {
-                Pair(it.first, it.second)
-            }
-        }.toList()
+        val edges = (1..m).map{readPairInts().run{
+            if (first < second) Pair(first, second) else Pair(second, first)
+        }}.toList()
         val edgeSet = edges.toSet()
         val missingEdge = findMissingEdge(n, edgeSet)
-        val ans = if (missingEdge == null) {
+        val (num, assign) = if (missingEdge == null) {
             constructPlanFromClique(n, edges)
         } else {
             missingEdge.run{constructPlan(n, edges, first, second)}
         }
-        println(ans.first)
-        println(ans.second.joinToString(" "))
+        println(num)
+        println(assign.joinToString(" "))
     }
 }
